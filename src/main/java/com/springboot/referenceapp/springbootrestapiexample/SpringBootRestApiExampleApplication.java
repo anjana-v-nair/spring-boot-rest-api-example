493cfcacf3383e5fea067812793d5f41accc8a2b
Anjana Nair <anjuviswan93@gmail.com>
2019-12-08 14:07:04 +0530

    scheduling and properties changes

--- src/main/java/com/springboot/referenceapp/springbootrestapiexample/SpringBootRestApiExampleApplication.java
+++ src/main/java/com/springboot/referenceapp/springbootrestapiexample/SpringBootRestApiExampleApplication.java
@@ -2,8 +2,10 @@ package com.springboot.referenceapp.springbootrestapiexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringBootRestApiExampleApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/springboot/referenceapp/springbootrestapiexample/schedular/SchedularService.java
+++ src/main/java/com/springboot/referenceapp/springbootrestapiexample/schedular/SchedularService.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class SchedularService {
 
-    @Scheduled(cron = "0 58 23 * * *", zone = "Asia/Calcutta")
+    @Scheduled(cron="0 03 14 * * *", zone="Asia/Calcutta")
     public void scheduledTask(){
-        System.out.println("This task will happen at 23:58 pm");
+        System.out.println("This task will happen at 14:03 pm");
     }
 }
